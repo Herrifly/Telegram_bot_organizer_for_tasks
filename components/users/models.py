498f531edf114a5ad46fb788263ec424ec858d96
Herrifly <nani-4545@mail.ru>
2023-07-04 16:32:10 +0300

    added relationship between tables

---
+++ components/users/models.py
@@ -0,0 +1,49 @@
+import sqlalchemy as sq
+from sqlalchemy.sql import text
+from sqlalchemy import Date
+from datetime import datetime
+from database.db_sync import Base, db_session
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    id: int = sq.Column(sq.Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    user_id: int = sq.Column(sq.Integer, nullable=False)
+    username: str = sq.Column(sq.String)
+    date_registration: Date = sq.Column(Date)
+
+    @classmethod
+    def insert(cls, user_id: int, username: str):
+        db_session.execute(
+            text(
+                """INSERT INTO users (user_id, username, date_registration)
+                VALUES (:user_id, :username, :date_registration);"""
+
+            ),
+            params={
+                'user_id': user_id,
+                'username': username,
+                'date_registration': datetime.now().date()
+            }
+        )
+
+        db_session.commit()
+
+
+    @classmethod
+    def select(cls, user_id: int):
+
+        result = db_session.execute(
+            text(
+                """SELECT user_id FROM users WHERE user_id = :user_id"""
+            ),
+            params={
+                'user_id': user_id
+            }
+        )
+
+        db_session.commit()
+
+        return result.fetchall()
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config import TOKEN
+from components.users.models import Users
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -15,7 +16,18 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    user_id = message.from_user.id
+
+    if len(Users.select(user_id)) == 0:
+        Users.insert(message.from_user.id, message.from_user.username)
+
+    await message.answer(f"Привет, {message.from_user.first_name}, я помогу тебе быть более продуктивным")
+
+
+@dp.message(Command('help'))
+async def cmd_help(message: types.Message):
+    await message.reply('Мой функционал:\n'
+                        '/tasks_waiting')
 
 
 # Запуск процесса поллинга новых апдейтов
